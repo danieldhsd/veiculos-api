916e1a835d10fb7807b5ff6182dcfd453664a9a7
Daniel Dutra <danieldhsd@gmail.com>
2021-04-25 20:28:57 -0300

    Commit

--- src/main/java/com/danieldhsd/veiculosapi/controller/VeiculoController.java
+++ src/main/java/com/danieldhsd/veiculosapi/controller/VeiculoController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,7 +23,6 @@ import com.danieldhsd.veiculosapi.model.Veiculo;
 import com.danieldhsd.veiculosapi.repository.VeiculosRepository;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/veiculos")
 public class VeiculoController {
 
@@ -32,7 +30,6 @@ public class VeiculoController {
 	private VeiculosRepository veiculosRepository;
 	
 	@GetMapping
-	@CrossOrigin
 	public List<Veiculo> getAllVeiculos() {
 		return veiculosRepository.findAll();
 	}
